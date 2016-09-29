ab48ad9caeaca0b63656e7a77955127566f84907
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2016-09-28 17:58:43 -0700

    functional gameplay for 5 turns, added a TODO for type validation

--- battleship.py
+++ battleship.py
@@ -1,14 +1,17 @@
 from random import randint
-# Building the board
 
+# Building the board
 board = []
 
-for x in range(0,5):
+for x in range(5):
     board.append(["O"] * 5)
 
 def print_board(board):
     for row in board:
         print " ".join(row)
+
+print "Let's play Battleship!"
+print_board(board)
 
 def random_row(board):
     return randint(0, len(board) - 1)
@@ -20,41 +23,27 @@ def random_col(board):
 
 ship_row = random_row(board)
 ship_col = random_col(board)
-'''For debugging:
-print ("Row = " + str(ship_row))
-print ("Column = " + str(ship_col))'''
 
-# Allowing for user guesses
-
-which_row = int(raw_input("Guess which row? ")) - 1
-which_col = int(raw_input("Guess which column? ")) - 1
-
-# Correct and Incorrect Guesses
-if guess_row == ship_row and guess_col == ship_col:
-    print("That's a hit! You sunk my battleship.")
-else:
-    print("You missed my battleship!")
-    board[guess_row][guess_col] = "X"
-    print_board(board)
-
-turn = 1
+# Game play for 5 turns
+turn = 0
 for turn in range(5):
-    print("This is turn number " + str(turn))
-    guess_row = int(raw_input("Guess Row:")) - 1
-    guess_col = int(raw_input("Guess Col:")) - 1
+    print("This is turn number " + str(turn + 1))
+    guess_row = int(raw_input("Guess a row: ")) - 1
+    guess_col = int(raw_input("Guess a column: ")) - 1
 
     if guess_row == ship_row and guess_col == ship_col:
         print "Congratulations! You sunk my battleship!"
-        return
+        break
     else:
         if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
-            print "Oops, that's not even in the ocean."
+            #TODO: Validate for NANs, empty strings, etc.
+            print "You're officially off-map."
         elif(board[guess_row][guess_col] == "X"):
             print "You guessed that one already."
         else:
             print "You missed my battleship!"
             board[guess_row][guess_col] = "X"
-            if turn == 4:
-                print("Game Over!")
         print_board(board)
+        if turn == 4:
+            print("Game Over!")
     turn += 1
